c8e02ad615b70baed16331c8dbd36b64dda8b2c5
Ryssa Teles Esmeraldo Sobreira <dig.ryssasobreira@opovo.local>
2023-11-09 12:50:23 -0300

    feat: criação das seções da landing page

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop:true,
+        margin:10,
+        nav:true,
+        autoWidth:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
